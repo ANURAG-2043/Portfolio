a5f992210134aea4cea5fc6831d8f8c1d40df7b6
ANURAG-2043 <itsaworkspace2043@gmail.com>
2025-04-25 17:00:32 +0530

    code modified

--- bundler/webpack.prod.js
+++ bundler/webpack.prod.js
@@ -1,6 +1,7 @@
 const { merge } = require('webpack-merge')
 const commonConfiguration = require('./webpack.common.js')
 const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const CopyWebpackPlugin = require('copy-webpack-plugin')
 
 module.exports = merge(
     commonConfiguration,
@@ -8,7 +9,12 @@ module.exports = merge(
         mode: 'production',
         plugins:
         [
-            new CleanWebpackPlugin()
+            new CleanWebpackPlugin(),
+            new CopyWebpackPlugin({
+                patterns: [
+                    { from: 'Loading_Page', to: 'Loading_Page' }
+                ]
+            })
         ]
     }
 )
